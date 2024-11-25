3ab8fbf6bd294b16b6df0a1979b203a485f623e3
rameshvelayudhan <45762390+rameshvelayudhan@users.noreply.github.com>
2024-11-25 17:15:53 +0530

    Update streamlit_app.py
    changs to move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -17,8 +16,8 @@ st.write(
 
 # st.write('You selected : ',  option)
 
-
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 nameonorder_string = ''
 
