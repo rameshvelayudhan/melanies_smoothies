593c2a1881862ad810f3c1a59261ffde193929bc
rameshvelayudhan <45762390+rameshvelayudhan@users.noreply.github.com>
2024-11-25 18:25:13 +0530

    Update streamlit_app.py
    Added dataframe to show json output

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,8 @@ ingredients_list = st.multiselect(
 # New section to display Smoothies nutrients information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+# st.text(smoothiefroot_response)
+sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width = True)
 
 if ingredients_list:
     st.write(ingredients_list)
